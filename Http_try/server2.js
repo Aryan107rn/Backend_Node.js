4c6febf1c2409cc4699d0b1bedf3a56f31018371
Aryan107rn <aryannakade123@gmail.com>
2026-08-31 01:41:22 +0530

    all tested at once ;)

--- Http_try/server2.js
+++ Http_try/server2.js
@@ -1,19 +1,31 @@
-// This is for http methods namely GET POST PUT PATCH DELETE 
-
 const http = require("http");
 
-const server = http.createServer((req,res)=>{
-   if(req.method === "GET" && req.url === "/users"){
-    res.end("Here are the user");
-   }
-   if (req.method === "POST" && req.url === "/users") {
-    res.end("Create a new user");
-}
-   else{
-    res.end("Nothing useful");
-   }
+const server = http.createServer((req, res) => {
+
+    if (req.method === "GET" && req.url === "/users") {
+        res.end("Get users");
+    }
+
+    else if (req.method === "POST" && req.url === "/users") {
+        res.end("Create user");
+    }
+
+    else if (req.method === "PUT" && req.url === "/users/10") {
+        res.end("Replace user 10");
+    }
+
+    else if (req.method === "PATCH" && req.url === "/users/10") {
+        res.end("Update user 10");
+    }
+
+    else if (req.method === "DELETE" && req.url === "/users/10") {
+        res.end("Delete user 10");
+    }
+
+    else {
+        res.end("404 Not Found");
+    }
+
 });
 
-server.listen(3000,()=>{
-    console.log("Server started on port 3000");
-});
+server.listen(8000);

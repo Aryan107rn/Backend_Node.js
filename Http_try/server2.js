4d0df2c24092eb8ae07b54e0168b55f098985fa7
Aryan107rn <aryannakade123@gmail.com>
2026-08-31 01:39:34 +0530

    tested server2.js

--- Http_try/server2.js
+++ Http_try/server2.js
@@ -6,8 +6,11 @@ const server = http.createServer((req,res)=>{
    if(req.method === "GET" && req.url === "/users"){
     res.end("Here are the user");
    }
+   else{
+    res.end("Nothing useful");
+   }
 });
 
-server.listen(8000,()=>{
-    console.log("Server started on port 8000");
+server.listen(3000,()=>{
+    console.log("Server started on port 3000");
 });
